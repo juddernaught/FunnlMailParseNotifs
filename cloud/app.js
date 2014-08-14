afe7a4f7461edb8d470bfe80d4fc786b3717488a
Daniel Judd <juddernaught@gmail.com>
2014-08-14 01:23:32 -0400

    name and subject inside push

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,5 @@
-
-// These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
+
 var app = express();
 
 var qs = require('querystring');
@@ -17,22 +16,35 @@ app.get('/hello', function(req, res) {
 });
 
 app.post('/send_notification', function(req, res) { 
+  var original = JSON.stringify(req.body.message_data);
+  //res.send(JSON.stringify(req.body.message_data));
+  var left_cut1 = original.substring(original.indexOf("addresses") + 25);
+  var left_cut2 = left_cut1.substring(left_cut1.indexOf("name") + 7);
+  var name = left_cut2.substring(0, left_cut2.indexOf("}") - 1);
+  
+
+  var left_subj = original.substring(original.indexOf("\"subject\"") + 11);
+  var subject = left_subj.substring(0, left_subj.indexOf(",\"folders\"") - 1);
+  
   Parse.Push.send({
-    channels: [ "webhook_id_" + req.body.webhook_id ],
+    channels: [ "account_id_" + req.body.account_id ],
     data: {
-      alert: "You've just received an important email!"
+      alert: name + "\n" + subject,
+      sound: "notifsound.m4a"
     }
   }, {
     success: function() {
+      res.send("success");
       // Push was successful
     },
     error: function(error) {
       // Handle error
     }
   }); 
-  console.log(req.body);
-  console.log(req.body.webhook_id);
-  console.log("webhook_id_" + req.body.webhook_id);
+  console.log("here2");
+  //iconsole.log(req.body.webhook_id);
+  
+  //console.log("webhook_id_" + req.body.webhook_id);*/
 });
 
 app.post('/failure', function(req, res) {
